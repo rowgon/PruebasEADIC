b7d07847653f8740dfdbf025c6852bf9b7f1b1e9
Roman <romandjgd0205@gmail.com>
2025-02-26 10:28:31 -0400

    Primer commit: Subiendo mi proyecto a GitHub

---
+++ pruebas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visualización de PDF</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+        iframe {
+            border: none;
+            width: 100%;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://eadic.com/wp-content/uploads/folletos/mo_mpuert_puertos_y_obras_maritimas_especiales.pdf#page=3&zoom=60"></iframe>
+</body>
+</html>
